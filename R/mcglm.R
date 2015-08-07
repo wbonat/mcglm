31d81e3a6f9623bdd067365e6c7272c8b0e1eaa8
Wagner Hugo Bonat <wbonat@ufpr.br>
2015-08-07 15:54:36 +0200

    Teste2

--- R/mcglm.R
+++ R/mcglm.R
@@ -1,8 +1,11 @@
 #' Fitting Multivariate Covariance Generalized Linear Models (McGLM)
 #'
 #' @description \code{mcglm} is used to fit multivariate covariance generalized linear models.
-#' The models are specified by a set of lists giving a symbolic description of the linear predictor and
-#' specyfing the link, variance and covariance functions.
+#' The models are specified by a set of lists giving a symbolic description of the linear predictor.
+#' The user can choose between a list of link, variance and covariance functions. The models are
+#' fitted using an estimating function approach, combining quasi-score functions for regression
+#' parameters and Pearson estimating function for covariance parameters. For details see Bonat and
+#' Jorgensen (2015).
 #'
 #' @param linear_pred A list of formula see \code{\link[stats]{formula}} for details.
 #' @param matrix_pred A list of known matrices to be used on the matrix linear predictor. Details
@@ -20,16 +23,14 @@
 #' @param control_initial A list of initial values for the fitting algorithm. See details below.
 #' @param control_algorithm A list of arguments to be passed for the fitting algorithm. See
 #' \code{\link[mcglm]{fit_mcglm}} for details.
-#' @param check_convergence Logical if TRUE provide a convergence check.
 #' @return mcglm returns an object of class "mcglm".
-
+#' @export
 mcglm <- function(linear_pred, matrix_pred,
                   link, variance, covariance,
                   offset, Ntrial, power_fixed,
                   data,
                   control_initial,
-                  control_algorithm = list(),
-                  check_convergence = FALSE){
+                  control_algorithm = list()) {
   con <- list("correct" = TRUE, "max_iter" = 20, "tol" = 1e-03,
        "method" = "chaser", "tunning" = 1, "verbose" = TRUE)
   con[(namc <- names(control_algorithm))] <- control_algorithm
@@ -37,7 +38,7 @@ mcglm <- function(linear_pred, matrix_pred,
   list_model_frame <- lapply(linear_pred, model.frame, data = data)
   list_Y <- lapply(list_model_frame, model.response)
   y_vec <- as.numeric(do.call(c, list_Y))
-  sparse <- lapply(matrix_pred, function(x){
+  sparse <- lapply(matrix_pred, function(x) {
     if(class(x) == "dgeMatrix"){FALSE} else TRUE})
   model_fit <- fit_mcglm(list_initial = control_initial,
                          list_link = link, list_variance = variance, list_covariance = covariance,
@@ -58,6 +59,8 @@ mcglm <- function(linear_pred, matrix_pred,
   model_fit$variance <- variance
   model_fit$covariance <- covariance
   model_fit$linear_pred <- linear_pred
+  model_fit$con <- con
+  model_fit$observed <- Matrix(y_vec, ncol = length(list_Y), nrow = dim(data)[1])
   class(model_fit) <- "mcglm"
   return(model_fit)
 }
