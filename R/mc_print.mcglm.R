4d12da262073c8d43f454098c40cf7f8959293c0
Walmes Zeviani <walmes@ufpr.br>
2015-09-30 20:57:50 -0300

    Apply 72 rule, add @author.

--- R/mc_print.mcglm.R
+++ R/mc_print.mcglm.R
@@ -1,16 +1,25 @@
-#' Print method for Multivariate Covariance Generalized Linear Model
+#' @title Print method for Multivariate Covariance Generalized Linear
+#'     Model
+#' @name print.mcglm
 #'
-#' @description The default print method for a mcglm object.
-#' @method print mcglm
+#' @description The default print method for a \code{mcglm} object.
+#'
 #' @param x fitted model objects of class mcglm as produced by mcglm().
 #' @param ... further arguments passed to or from other methods.
-#' @export
+#'
+#' @author Wagner Hugo Bonat, \email{wbonat@@ufpr.br}
+#'
 #' @rdname print.mcglm
+#' @method print mcglm
+#' @export
 
 print.mcglm <- function(x, ...) {
     object <- x
     n_resp <- length(object$beta_names)
-    regression <- mc_updateBeta(list_initial = list(), betas = object$Regression, information = object$Information, n_resp = n_resp)
+    regression <- mc_updateBeta(list_initial = list(),
+                                betas = object$Regression,
+                                information = object$Information,
+                                n_resp = n_resp)
     for (i in 1:n_resp) {
         cat("Call: ")
         print(object$linear_pred[[i]])
