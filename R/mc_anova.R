577c2fcfe712a230a13d9cceae9f488e03c8d40b
Walmes Zeviani <walmes@ufpr.br>
2015-09-30 19:37:52 -0300

    Add @return and @author, apply 72 rule.

--- R/mc_anova.R
+++ R/mc_anova.R
@@ -1,10 +1,18 @@
-#' ANOVA method for McGLMs.
+#' @title ANOVA method for McGLMs.
 #'
-#' @description ANOVA method for McGLMS.
+#' @description ANOVA method for object of class McGLMS.
 #'
-#' @param object an object of class mcglm, usually, a result of a call to \code{mcglm}.
-#' @param ... additional arguments affecting the summary produced. Note that there is no extra options for
-#' mcglm object class.
+#' @param object an object of class \code{mcglm}, usually, a result of a
+#'     call to \code{mcglm()}.
+#' @param ... additional arguments affecting the summary produced. Note
+#'     that there is no extra options for mcglm object class.
+#'
+#' @return A \code{data.frame} with Chi-square statistic to test the
+#'     null hypothesis of a parameter, or a set of parameters, be
+#'     zero. The Wald test based on the observed covariance matrix of
+#'     the parameters is used.
+#'
+#' @author Wagner Hugo Bonat, \email{wbonat@@ufpr.br}
 #' @export
 
 anova.mcglm <- function(object, ...) {
@@ -24,7 +32,8 @@ anova.mcglm <- function(object, ...) {
     for (i in 1:n_resp) {
         idx.id = idx.vec == i
         temp.vcov[[i]] <- vv[idx.id, idx.id]
-        temp.beta[[i]] <- coef(object, type = "beta", response = i)$Estimates
+        temp.beta[[i]] <-
+            coef(object, type = "beta", response = i)$Estimates
     }
     saida <- list()
     for (i in 1:n_resp) {
@@ -40,13 +49,20 @@ anova.mcglm <- function(object, ...) {
         X2.resp <- list()
         for (j in 1:n_terms) {
             idx.TF <- idx == j
-            temp <- as.numeric(t(temp.beta[[i]][idx.TF]) %*% solve(as.matrix(temp.vcov[[i]])[idx.TF, idx.TF]) %*% temp.beta[[i]][idx.TF])
+            temp <- as.numeric(
+                t(temp.beta[[i]][idx.TF]) %*%
+                    solve(as.matrix(temp.vcov[[i]])[idx.TF, idx.TF]) %*%
+                    temp.beta[[i]][idx.TF])
             nbeta.test <- length(temp.beta[[i]][idx.TF])
-            X2.resp[[j]] <- data.frame(Covariate = names[idx.TF][1], Chi.Square = round(temp, 4), Df = nbeta.test, p.value = round(pchisq(temp,
-                nbeta.test, lower.tail = FALSE), 4))
+            X2.resp[[j]] <- data.frame(
+                Covariate = names[idx.TF][1],
+                Chi.Square = round(temp, 4),
+                Df = nbeta.test,
+                p.value = round(pchisq(temp, nbeta.test,
+                                       lower.tail = FALSE), 4))
         }
         saida[[i]] <- do.call(rbind, X2.resp)
     }
-    cat("Wald test for fixed effects", "\n")
+    cat("Wald test for fixed effects\n")
     return(saida)
 }
