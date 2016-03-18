6177d0d921212158d5462936a1322112b976242d
wbonat <wbonat@gmail.com>
2016-03-18 19:28:52 +0100

    Include Pseudo loglikelihood(plogLik) and the generalized error sum of squares (ESS)

---
+++ R/mc_ess.R
@@ -0,0 +1,35 @@
+#' @title Extract generalized error sum of squares (ESS) for multivariate
+#'     covariance generalized linear models.
+#' @author Wagner Hugo Bonat
+#'
+#' @description Extract the generalized error sum of squares for a fitted
+#' McGLM.
+#' @param object an object or a list of objects representing a model
+#' of \code{mcglm} class.
+#' @param verbose Logical
+#' @return Returns the value of the Gaussian pseudo-loglikelihood.
+#' @export
+
+ESS <- function(object, verbose = TRUE) {
+  if(class(object) == "mcglm") {
+    b <- c(as.matrix(object$observed)) - c(as.matrix(object$fitted))
+    ess <- as.numeric(t(b)%*%object$inv_C%*%b)
+    df <- length(coef(object)$Estimates)
+    ess <- ess/df
+    if (verbose) cat("ESS", ess)
+    invisible(list("ESS" = ess))
+  }
+  if(class(object) == "list") {
+    Y <- do.call(c,lapply(object, function(x)as.numeric(x$observed)))
+    mu <-do.call(c,lapply(object, function(x)as.numeric(x$fitted)))
+    b <- Y - mu
+    C.list <- lapply(object, function(x)x$C)
+    inv_C.list <- lapply(object, function(x)x$inv_C)
+    inv_C <- bdiag(inv_C.list)
+    ess <- as.numeric(t(b)%*%inv_C%*%b)
+    df <- sum(unlist(lapply(object, function(x)length(coef(x)$Estimates))))
+    ess <- ess/df
+    if (verbose) cat("ESS", ess)
+    invisible(list("ESS" = ess))
+  }
+}
---
+++ R/mc_plogLik.R
@@ -0,0 +1,45 @@
+#' @title Extract Gaussian pseudo-loglikelihood (plogLik) for multivariate
+#'     covariance generalized linear models.
+#' @author Wagner Hugo Bonat
+#'
+#' @description Extract the Gaussian pseudo-loglikelihood for a fitted
+#' McGLM.
+#' @param object an object or a list of objects representing a model
+#' of \code{mcglm} class.
+#' @param verbose Logical
+#' @return Returns the value of the Gaussian pseudo-loglikelihood.
+#' @export
+
+plogLik <- function(object, verbose = TRUE) {
+    my.gauss <- function(b, det.Sigma, inv.Sigma){
+        n <- length(b)
+        dens <- (-n/2)*log(2*pi) - 0.5*det.Sigma -
+            0.5*t(b)%*%inv.Sigma%*%b
+        return(dens)
+    }
+    if(class(object) == "mcglm") {
+    b <- c(as.matrix(object$observed)) - c(as.matrix(object$fitted))
+    det.Sigma <- determinant(object$C)$modulus
+    ll <- round(as.numeric(my.gauss(b = b, det.Sigma = det.Sigma,
+                              inv.Sigma = object$inv_C)),2)
+    df <- length(coef(object)$Estimates)
+    if (verbose) cat("Pseudo log Lik.", ll, sprintf("(df=%d)",df))
+    invisible(list("plogLik" = ll, "df" = df))
+    }
+    if(class(object) == "list") {
+      Y <- do.call(c,lapply(object, function(x)as.numeric(x$observed)))
+      mu <-do.call(c,lapply(object, function(x)as.numeric(x$fitted)))
+      b <- Y - mu
+      C.list <- lapply(object, function(x)x$C)
+      C <- bdiag(C.list)
+      inv_C.list <- lapply(object, function(x)x$inv_C)
+      inv_C <- bdiag(inv_C.list)
+      det.Sigma <- determinant(C)$modulus
+      ll <- round(as.numeric(my.gauss(b = b, det.Sigma = det.Sigma,
+                                      inv.Sigma = inv_C)),2)
+      df <- sum(unlist(lapply(object, function(x)length(coef(x)$Estimates))))
+      if (verbose) cat("Pseudo log Lik.", ll, sprintf("(df=%d)",df))
+      invisible(list("plogLik" = ll, "df" = df))
+    }
+}
+
