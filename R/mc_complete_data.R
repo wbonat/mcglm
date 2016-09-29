9d59c50a806b7919ae6ccbd255bea221c936531d
wbonat <wbonat@gmail.com>
2016-09-29 11:28:10 +0200

    Handling missing data.

---
+++ R/mc_complete_data.R
@@ -0,0 +1,42 @@
+#' @title Complete Data Set with NA
+#' @author Wagner Hugo Bonat, \email{wbonat@@ufpr.br}
+#'
+#' @description The function \code{mc_complete_data} completes a data
+#' set with NA values for helping to construct the components of the
+#' matrix linear predictor in models that require equal number of
+#' observations by subjects (id).
+#'
+#' @param data a data.frame to be completed with NA.
+#' @param id name of the column (string) containing the subject id.
+#' @param index name of the column (string) containing the index to be completed.
+#' @param id.exp how the index is expected to be for all subjects.
+#' @return A data.frame with the same number of observations by subject.
+#'
+#' @source Bonat, W. H. (2016). Multiple Response Variables Regression
+#' Models in R: The mcglm Package. Journal of Statistical Software, submitted.
+#'
+#' @seealso \code{mc_dglm}, \code{mc_ns}, \code{mc_ma} and \code{mc_rw}.
+#' @export
+
+
+mc_complete_data <- function(data, id, index, id.exp) {
+  data.id <- split(data, data[id])
+  ncol <- dim(data)[2]
+  NN <- rep(NA, ncol)
+  temp_list <- list()
+  n.id <- length(data.id)
+  for(j in 1:n.id) {
+    temp <- data.frame(matrix(NA,length(id.exp),ncol))
+    for(i in 1:length(id.exp)) {
+      data_temp <- data.id[[j]][data.id[[j]][index] == id.exp[i],]
+      if(nrow(data_temp) == 0) {temp[i,] <- NN}
+      if(nrow(data_temp) != 0) {temp[i,] <- c(data_temp)}
+    }
+    temp_list[[j]] <- temp
+  }
+  data_complete <- do.call(rbind, temp_list)
+  names(data_complete) <- names(data)
+  data_complete[index] <- rep(id.exp, n.id)
+  data_complete[id] <- rep(id.exp, each = n.id)
+  return(data_complete)
+}
---
+++ R/mc_remove_na.R
@@ -0,0 +1,24 @@
+#' @title Remove NA from Matrix Linear Predictor
+#' @author Wagner Hugo Bonat, \email{wbonat@@ufpr.br}
+#'
+#' @description The function \code{mc_remove_na} removes NA from each
+#' component of the matrix linear predictor. It is in general used after
+#' the function \code{mc_complete_data}.
+#'
+#' @param matrix_pred a list of known matrices.
+#' @param cod index indicating the columns should be removed.
+#' @return A list of matrices.
+#'
+#' @source Bonat, W. H. (2016). Multiple Response Variables Regression
+#' Models in R: The mcglm Package. Journal of Statistical Software, submitted.
+#'
+#' @seealso \code{mc_dglm}, \code{mc_ns}, \code{mc_ma} and \code{mc_rw}.
+#' @export
+
+mc_remove_na <- function(matrix_pred, cod) {
+  matrix_pred_temp <- list()
+  for(i in 1:length(matrix_pred)) {
+    matrix_pred_temp[[i]] <- matrix_pred[[i]][-cod,-cod]
+  }
+  return(matrix_pred_temp)
+}
